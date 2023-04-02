48efe2b371879e0a8c96fc342bd04f53a38242d2
karlnh <hubigkarl@gmail.com>
2023-04-02 00:15:06 -0500

    make update category by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,9 +41,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  
+  try {
+    Category.update({
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    })
+    .then((updatedCategory) => {
+      res.json(updatedCategory);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
