c7d5459de8eb64492fa26275bb35fd3c9c711e29
karlnh <hubigkarl@gmail.com>
2023-04-02 00:06:13 -0500

    make post new category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,12 +29,21 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(newCategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
